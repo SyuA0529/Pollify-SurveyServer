cae1e1d0f9df75be7656a609aa07fbe29cdc0439
SyuA0529 <silbi3408@gmail.com>
2023-06-05 03:07:23 +0900

    refactor: remove unused import statement

--- src/main/java/dku/cloudcomputing/surveyserver/security/SecurityConfig.java
+++ src/main/java/dku/cloudcomputing/surveyserver/security/SecurityConfig.java
@@ -2,27 +2,18 @@ package dku.cloudcomputing.surveyserver.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dku.cloudcomputing.surveyserver.controller.dto.StatusResponseDto;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.io.IOException;
 
 @Configuration
 @RequiredArgsConstructor
